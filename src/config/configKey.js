2f21017a141a7b465d8b859088c33c5103d9ad60
alisheeraz007 <alisheera007@gmail.com>
2020-03-19 16:38:37 +0500

    a

--- src/config/configKey.js
+++ src/config/configKey.js
@@ -1,12 +1,12 @@
 const config = {
-    apiKey: "AIzaSyCDd668gy-HFMwdfQjx3WOanyjbmRVYQ_c",
-    authDomain: "jewelery-web.firebaseapp.com",
-    databaseURL: "https://jewelery-web.firebaseio.com",
-    projectId: "jewelery-web",
-    storageBucket: "jewelery-web.appspot.com",
-    messagingSenderId: "143884647439",
-    appId: "1:143884647439:web:b735b62005a5a6e32d377a",
-    measurementId: "G-8C6D58Z348"
+    apiKey: "AIzaSyDAteGOp_6dqQ4fHA53cktyQn7HUk8unR0",
+    authDomain: "portfolioo2.firebaseapp.com",
+    databaseURL: "https://portfolioo2.firebaseio.com",
+    projectId: "portfolioo2",
+    storageBucket: "portfolioo2.appspot.com",
+    messagingSenderId: "822529207999",
+    appId: "1:822529207999:web:1d72d92d1d4e1f31ed600a",
+    measurementId: "G-4MWGJQRVQE"
 };
 
 export default config;
